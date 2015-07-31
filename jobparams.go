c605d0dec867f2802cb6e060fd70df64a364e138
Sergey Romanov <xxsmotur@gmail.com>
2015-07-31 23:48:30 +0500

    Add comments

--- jobparams.go
+++ jobparams.go
@@ -13,6 +13,7 @@ type JobParams struct {
 }
 
 
+//getJobArguments needs for convert item data for the JobParams (worker area)
 func getJobArguments(item string) JobParams {
 	var params JobParams
 	err := json.Unmarshal([]byte(item), &params)
@@ -23,6 +24,7 @@ func getJobArguments(item string) JobParams {
 	return params
 }
 
+//prepareParemeters converts JobParams object to the string
 func prepareParameters(jp *JobParams) string {
 	results, err := json.Marshal(jp)
 	if err != nil {
