fc876cb98f90f348f83ea5957e04edf8456a8d73
Sergey Romanov <xxsmotur@gmail.com>
2015-08-02 21:56:46 +0500

    Add RPC module

---
+++ rpc.go
@@ -0,0 +1,40 @@
+package samovar
+
+import (
+    "net"
+	"net/rpc"
+	"log"
+)
+
+type RPCData struct {
+	address string
+}
+
+const (
+	ADDR = ":9873"
+)
+
+func RegisterRPCFunction(obj interface{}){
+	rpc.Register(obj)
+}
+
+func InitRPC(address string) *RPCData {
+	rpcdata := new(RPCData)
+	if address == "" {
+		address = ADDR
+	}
+
+	rpcdata.address = address
+	return rpcdata
+}
+
+func (rpcdata *RPCData) Run() {
+	go func() {
+		l, e := net.Listen("tcp", rpcdata.address)
+		if e != nil {
+			log.Fatal("listen error:", e)
+		}
+
+		rpc.Accept(l)
+	}()
+}
