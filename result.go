4ea1f4b7c4b9fcba1ed12115ab5a5ca3a88a3167
Sergey Romanov <xxsmotur@gmail.com>
2015-08-13 02:29:40 +0500

    Result: Update

--- result.go
+++ result.go
@@ -4,26 +4,22 @@ import (
 	"encoding/json"
 	"fmt"
 	"gopkg.in/redis.v3"
+	"hash/crc32"
 	"log"
 	"time"
-	"hash/crc32"
 )
 
 type Result struct {
-	Title  string
-	Result interface{}
-	Date   time.Time
+	Title        string
+	Result       interface{}
+	Date         time.Time
 	DataChecksum uint32
 }
 
 //Store result provides write function result to db
 func (res *Result) storeResult(client *redis.Client) {
-	results, err := json.Marshal(res)
-	if err != nil {
-		panic(err)
-	}
-
-	client.HSet("samovar", res.Title, string(results))
+	results := Marshal(res)
+	client.HSet("samovar", fmt.Sprintf("%s_result", res.Title), string(results))
 }
 
 func getResult(client *redis.Client, title string) interface{} {
@@ -42,6 +38,6 @@ func getResult(client *redis.Client, title string) interface{} {
 
 func getChecksum(item []byte) uint32 {
 	crc := crc32.New(crc32.MakeTable(crc32.Castagnoli))
-    crc.Write([]byte(item))
-    return crc.Sum32()
+	crc.Write([]byte(item))
+	return crc.Sum32()
 }
