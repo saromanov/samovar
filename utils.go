6f012616cff222b56abc3268eee3068d2b436575
Sergey Romanov <xxsmotur@gmail.com>
2015-08-13 02:27:36 +0500

    Add marshal params to utils

--- utils.go
+++ utils.go
@@ -1,18 +1,26 @@
 package samovar
 
-import
-(
+import (
+	"fmt"
 	"os"
 	"os/signal"
-	"fmt"
+	"encoding/json"
 )
 
-func detectExit(){
+func detectExit() {
 	sig := make(chan os.Signal, 1)
 	signal.Notify(sig, os.Interrupt)
-	go func(){
-        <-sig
-        fmt.Println("Good bye!")
-        os.Exit(1)
+	go func() {
+		<-sig
+		fmt.Println("Good bye!")
+		os.Exit(1)
 	}()
+}
+
+func Marshal(res interface{}) string {
+	results, err := json.Marshal(res)
+	if err != nil {
+		panic(err)
+	}
+	return string(results)
 }
