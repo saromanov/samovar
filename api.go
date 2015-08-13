47dd49909a7615cef847de22b0210ae325503b03
Sergey Romanov <xxsmotur@gmail.com>
2015-08-13 22:30:20 +0500

    Add method for starting new job

--- api.go
+++ api.go
@@ -8,8 +8,17 @@ import
 )
 
 
+type JobShow struct {
+
+}
+
 func StartServer(jobs *Jobs) {
 	r := mux.NewRouter()
+	client := new(Client)
+	client.backend = InitRedisBackend()
+	client.client = initRedis("localhost:6379")
+
+	//Get information about job
 	r.HandleFunc("/statjob/{title}", func(w http.ResponseWriter, req *http.Request){
 		title := mux.Vars(req)["title"]
 		var job Job
@@ -19,6 +28,16 @@ func StartServer(jobs *Jobs) {
 		}
 		w.Write([]byte(fmt.Sprintf("%d", job.numberofcalls)))
 		return
+	})
+
+	//Start new job
+	r.HandleFunc("/jobrun/{title}", func(w http.ResponseWriter, req *http.Request) {
+		title := mux.Vars(req)["title"]
+		if title == "" {
+			return
+		}
+
+		client.Send(title, &JobOptions{})
 	})
 	http.ListenAndServe(":8000", r)
 }
